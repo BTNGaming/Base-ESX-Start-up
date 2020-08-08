4ee2f102e67f0a10924bdeb3f80afc904ec3912e
BTNGaming <35198559+BTNGaming@users.noreply.github.com>
2020-08-07 17:36:20 -0700

    Create esx-base-server.sql

---
+++ esx-base-server.sql
@@ -0,0 +1,266 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.11-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.0.0.5919
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for essentialmode
+CREATE DATABASE IF NOT EXISTS `essentialmode` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */;
+USE `essentialmode`;
+
+-- Dumping structure for table essentialmode.addon_account
+CREATE TABLE IF NOT EXISTS `addon_account` (
+  `name` varchar(60) COLLATE utf8mb4_bin NOT NULL,
+  `label` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `shared` int(11) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Dumping data for table essentialmode.addon_account: ~0 rows (approximately)
+DELETE FROM `addon_account`;
+/*!40000 ALTER TABLE `addon_account` DISABLE KEYS */;
+/*!40000 ALTER TABLE `addon_account` ENABLE KEYS */;
+
+-- Dumping structure for table essentialmode.addon_account_data
+CREATE TABLE IF NOT EXISTS `addon_account_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_name` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `money` double NOT NULL,
+  `owner` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Dumping data for table essentialmode.addon_account_data: ~0 rows (approximately)
+DELETE FROM `addon_account_data`;
+/*!40000 ALTER TABLE `addon_account_data` DISABLE KEYS */;
+/*!40000 ALTER TABLE `addon_account_data` ENABLE KEYS */;
+
+-- Dumping structure for table essentialmode.addon_inventory
+CREATE TABLE IF NOT EXISTS `addon_inventory` (
+  `name` varchar(60) COLLATE utf8mb4_bin NOT NULL,
+  `label` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `shared` int(11) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Dumping data for table essentialmode.addon_inventory: ~0 rows (approximately)
+DELETE FROM `addon_inventory`;
+/*!40000 ALTER TABLE `addon_inventory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `addon_inventory` ENABLE KEYS */;
+
+-- Dumping structure for table essentialmode.addon_inventory_items
+CREATE TABLE IF NOT EXISTS `addon_inventory_items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `inventory_name` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `count` int(11) NOT NULL,
+  `owner` varchar(60) COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Dumping data for table essentialmode.addon_inventory_items: ~0 rows (approximately)
+DELETE FROM `addon_inventory_items`;
+/*!40000 ALTER TABLE `addon_inventory_items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `addon_inventory_items` ENABLE KEYS */;
+
+-- Dumping structure for table essentialmode.billing
+CREATE TABLE IF NOT EXISTS `billing` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(40) COLLATE utf8mb4_bin NOT NULL,
+  `sender` varchar(40) COLLATE utf8mb4_bin NOT NULL,
+  `target_type` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `target` varchar(40) COLLATE utf8mb4_bin NOT NULL,
+  `label` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `amount` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Dumping data for table essentialmode.billing: ~0 rows (approximately)
+DELETE FROM `billing`;
+/*!40000 ALTER TABLE `billing` DISABLE KEYS */;
+/*!40000 ALTER TABLE `billing` ENABLE KEYS */;
+
+-- Dumping structure for table essentialmode.characters
+CREATE TABLE IF NOT EXISTS `characters` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `firstname` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `lastname` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `dateofbirth` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `sex` varchar(1) COLLATE utf8mb4_bin NOT NULL DEFAULT 'M',
+  `height` varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Dumping data for table essentialmode.characters: ~0 rows (approximately)
+DELETE FROM `characters`;
+/*!40000 ALTER TABLE `characters` DISABLE KEYS */;
+/*!40000 ALTER TABLE `characters` ENABLE KEYS */;
+
+-- Dumping structure for table essentialmode.datastore
+CREATE TABLE IF NOT EXISTS `datastore` (
+  `name` varchar(60) COLLATE utf8mb4_bin NOT NULL,
+  `label` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `shared` int(11) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Dumping data for table essentialmode.datastore: ~0 rows (approximately)
+DELETE FROM `datastore`;
+/*!40000 ALTER TABLE `datastore` DISABLE KEYS */;
+/*!40000 ALTER TABLE `datastore` ENABLE KEYS */;
+
+-- Dumping structure for table essentialmode.datastore_data
+CREATE TABLE IF NOT EXISTS `datastore_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) COLLATE utf8mb4_bin NOT NULL,
+  `owner` varchar(60) COLLATE utf8mb4_bin DEFAULT NULL,
+  `data` longtext COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_datastore_data_name_owner` (`name`,`owner`),
+  KEY `index_datastore_data_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Dumping data for table essentialmode.datastore_data: ~0 rows (approximately)
+DELETE FROM `datastore_data`;
+/*!40000 ALTER TABLE `datastore_data` DISABLE KEYS */;
+/*!40000 ALTER TABLE `datastore_data` ENABLE KEYS */;
+
+-- Dumping structure for table essentialmode.items
+CREATE TABLE IF NOT EXISTS `items` (
+  `name` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `label` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `limit` int(11) NOT NULL DEFAULT -1,
+  `rare` int(11) NOT NULL DEFAULT 0,
+  `can_remove` int(11) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Dumping data for table essentialmode.items: ~0 rows (approximately)
+DELETE FROM `items`;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+
+-- Dumping structure for table essentialmode.jobs
+CREATE TABLE IF NOT EXISTS `jobs` (
+  `name` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `label` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `whitelisted` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Dumping data for table essentialmode.jobs: ~1 rows (approximately)
+DELETE FROM `jobs`;
+/*!40000 ALTER TABLE `jobs` DISABLE KEYS */;
+INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
+	('unemployed', 'Unemployed', 0);
+/*!40000 ALTER TABLE `jobs` ENABLE KEYS */;
+
+-- Dumping structure for table essentialmode.job_grades
+CREATE TABLE IF NOT EXISTS `job_grades` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `job_name` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `grade` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `label` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `salary` int(11) NOT NULL,
+  `skin_male` longtext COLLATE utf8mb4_bin NOT NULL,
+  `skin_female` longtext COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Dumping data for table essentialmode.job_grades: ~8 rows (approximately)
+DELETE FROM `job_grades`;
+/*!40000 ALTER TABLE `job_grades` DISABLE KEYS */;
+INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+	(1, 'unemployed', 0, 'unemployed', 'Unemployed', 200, '{}', '{}'),
+	(2, 'lumberjack', 0, 'employee', 'Employee', 0, '{}', '{}'),
+	(3, 'fisherman', 0, 'employee', 'Employee', 0, '{}', '{}'),
+	(4, 'fueler', 0, 'employee', 'Employee', 0, '{}', '{}'),
+	(5, 'reporter', 0, 'employee', 'Employee', 0, '{}', '{}'),
+	(6, 'tailor', 0, 'employee', 'Employee', 0, '{"mask_1":0,"arms":1,"glasses_1":0,"hair_color_2":4,"makeup_1":0,"face":19,"glasses":0,"mask_2":0,"makeup_3":0,"skin":29,"helmet_2":0,"lipstick_4":0,"sex":0,"torso_1":24,"makeup_2":0,"bags_2":0,"chain_2":0,"ears_1":-1,"bags_1":0,"bproof_1":0,"shoes_2":0,"lipstick_2":0,"chain_1":0,"tshirt_1":0,"eyebrows_3":0,"pants_2":0,"beard_4":0,"torso_2":0,"beard_2":6,"ears_2":0,"hair_2":0,"shoes_1":36,"tshirt_2":0,"beard_3":0,"hair_1":2,"hair_color_1":0,"pants_1":48,"helmet_1":-1,"bproof_2":0,"eyebrows_4":0,"eyebrows_2":0,"decals_1":0,"age_2":0,"beard_1":5,"shoes":10,"lipstick_1":0,"eyebrows_1":0,"glasses_2":0,"makeup_4":0,"decals_2":0,"lipstick_3":0,"age_1":0}', '{"mask_1":0,"arms":5,"glasses_1":5,"hair_color_2":4,"makeup_1":0,"face":19,"glasses":0,"mask_2":0,"makeup_3":0,"skin":29,"helmet_2":0,"lipstick_4":0,"sex":1,"torso_1":52,"makeup_2":0,"bags_2":0,"chain_2":0,"ears_1":-1,"bags_1":0,"bproof_1":0,"shoes_2":1,"lipstick_2":0,"chain_1":0,"tshirt_1":23,"eyebrows_3":0,"pants_2":0,"beard_4":0,"torso_2":0,"beard_2":6,"ears_2":0,"hair_2":0,"shoes_1":42,"tshirt_2":4,"beard_3":0,"hair_1":2,"hair_color_1":0,"pants_1":36,"helmet_1":-1,"bproof_2":0,"eyebrows_4":0,"eyebrows_2":0,"decals_1":0,"age_2":0,"beard_1":5,"shoes":10,"lipstick_1":0,"eyebrows_1":0,"glasses_2":0,"makeup_4":0,"decals_2":0,"lipstick_3":0,"age_1":0}'),
+	(7, 'miner', 0, 'employee', 'Employee', 0, '{"tshirt_2":1,"ears_1":8,"glasses_1":15,"torso_2":0,"ears_2":2,"glasses_2":3,"shoes_2":1,"pants_1":75,"shoes_1":51,"bags_1":0,"helmet_2":0,"pants_2":7,"torso_1":71,"tshirt_1":59,"arms":2,"bags_2":0,"helmet_1":0}', '{}'),
+	(8, 'slaughterer', 0, 'employee', 'Employee', 0, '{"age_1":0,"glasses_2":0,"beard_1":5,"decals_2":0,"beard_4":0,"shoes_2":0,"tshirt_2":0,"lipstick_2":0,"hair_2":0,"arms":67,"pants_1":36,"skin":29,"eyebrows_2":0,"shoes":10,"helmet_1":-1,"lipstick_1":0,"helmet_2":0,"hair_color_1":0,"glasses":0,"makeup_4":0,"makeup_1":0,"hair_1":2,"bproof_1":0,"bags_1":0,"mask_1":0,"lipstick_3":0,"chain_1":0,"eyebrows_4":0,"sex":0,"torso_1":56,"beard_2":6,"shoes_1":12,"decals_1":0,"face":19,"lipstick_4":0,"tshirt_1":15,"mask_2":0,"age_2":0,"eyebrows_3":0,"chain_2":0,"glasses_1":0,"ears_1":-1,"bags_2":0,"ears_2":0,"torso_2":0,"bproof_2":0,"makeup_2":0,"eyebrows_1":0,"makeup_3":0,"pants_2":0,"beard_3":0,"hair_color_2":4}', '{"age_1":0,"glasses_2":0,"beard_1":5,"decals_2":0,"beard_4":0,"shoes_2":0,"tshirt_2":0,"lipstick_2":0,"hair_2":0,"arms":72,"pants_1":45,"skin":29,"eyebrows_2":0,"shoes":10,"helmet_1":-1,"lipstick_1":0,"helmet_2":0,"hair_color_1":0,"glasses":0,"makeup_4":0,"makeup_1":0,"hair_1":2,"bproof_1":0,"bags_1":0,"mask_1":0,"lipstick_3":0,"chain_1":0,"eyebrows_4":0,"sex":1,"torso_1":49,"beard_2":6,"shoes_1":24,"decals_1":0,"face":19,"lipstick_4":0,"tshirt_1":9,"mask_2":0,"age_2":0,"eyebrows_3":0,"chain_2":0,"glasses_1":5,"ears_1":-1,"bags_2":0,"ears_2":0,"torso_2":0,"bproof_2":0,"makeup_2":0,"eyebrows_1":0,"makeup_3":0,"pants_2":0,"beard_3":0,"hair_color_2":4}');
+/*!40000 ALTER TABLE `job_grades` ENABLE KEYS */;
+
+-- Dumping structure for table essentialmode.society_moneywash
+CREATE TABLE IF NOT EXISTS `society_moneywash` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(60) COLLATE utf8mb4_bin NOT NULL,
+  `society` varchar(60) COLLATE utf8mb4_bin NOT NULL,
+  `amount` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Dumping data for table essentialmode.society_moneywash: ~0 rows (approximately)
+DELETE FROM `society_moneywash`;
+/*!40000 ALTER TABLE `society_moneywash` DISABLE KEYS */;
+/*!40000 ALTER TABLE `society_moneywash` ENABLE KEYS */;
+
+-- Dumping structure for table essentialmode.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `identifier` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `license` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `money` int(11) DEFAULT NULL,
+  `name` varchar(50) COLLATE utf8mb4_bin DEFAULT '',
+  `skin` longtext COLLATE utf8mb4_bin DEFAULT NULL,
+  `job` varchar(50) COLLATE utf8mb4_bin DEFAULT 'unemployed',
+  `job_grade` int(11) DEFAULT 0,
+  `loadout` longtext COLLATE utf8mb4_bin DEFAULT NULL,
+  `position` varchar(36) COLLATE utf8mb4_bin DEFAULT NULL,
+  `bank` int(11) DEFAULT NULL,
+  `permission_level` int(11) DEFAULT NULL,
+  `group` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `status` longtext COLLATE utf8mb4_bin DEFAULT NULL,
+  `firstname` varchar(50) COLLATE utf8mb4_bin DEFAULT '',
+  `lastname` varchar(50) COLLATE utf8mb4_bin DEFAULT '',
+  `dateofbirth` varchar(25) COLLATE utf8mb4_bin DEFAULT '',
+  `sex` varchar(10) COLLATE utf8mb4_bin DEFAULT '',
+  `height` varchar(5) COLLATE utf8mb4_bin DEFAULT '',
+  PRIMARY KEY (`identifier`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Dumping data for table essentialmode.users: ~0 rows (approximately)
+DELETE FROM `users`;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+-- Dumping structure for table essentialmode.user_accounts
+CREATE TABLE IF NOT EXISTS `user_accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(22) COLLATE utf8mb4_bin NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `money` double NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Dumping data for table essentialmode.user_accounts: ~0 rows (approximately)
+DELETE FROM `user_accounts`;
+/*!40000 ALTER TABLE `user_accounts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_accounts` ENABLE KEYS */;
+
+-- Dumping structure for table essentialmode.user_inventory
+CREATE TABLE IF NOT EXISTS `user_inventory` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(22) COLLATE utf8mb4_bin NOT NULL,
+  `item` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `count` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Dumping data for table essentialmode.user_inventory: ~0 rows (approximately)
+DELETE FROM `user_inventory`;
+/*!40000 ALTER TABLE `user_inventory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_inventory` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
